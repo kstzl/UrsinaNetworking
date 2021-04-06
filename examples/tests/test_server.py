7c4bbd8e0673caa5175d77f8ab0ea6cca1bfe8fa
kstzl <78973002+kstzl@users.noreply.github.com>
2021-04-06 05:14:19 +0200

    Delete examples directory

--- examples/tests/test_server.py
+++
@@ -1,9 +0,0 @@
-from UrsinaNetworking import UrsinaNetworkingServer
-
-Server = UrsinaNetworkingServer("localhost", 25565)
-
-@Server.event
-def HelloFromClient(Player, C):
-    print(f"Receveid hello from {Player}")
-    Player.send_message("HelloFromServer", f"Hey {Player} how are you ? :D")
-    Server.broadcast("Broadcast", Server.get_clients_ids())
