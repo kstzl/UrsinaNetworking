2251c1ab36b1d6bfa14476fd7eb84e26c6a005f5
Kevin <kevin.stoetzel@gmail.com>
2021-04-06 09:05:46 +0200

    fixed send file

--- examples/send_files/send_file_client.py
+++ examples/send_files/send_file_client.py
@@ -1,4 +1,5 @@
 from UrsinaNetworking import UrsinaNetworkingClient
+from UrsinaNetworking import ursina_networking_decompress_file
 
 Client = UrsinaNetworkingClient("localhost", 25565)
 
@@ -6,6 +7,6 @@ Client = UrsinaNetworkingClient("localhost", 25565)
 def ReceiveFile(C):
     print(C)
     f = open("recv.png", "wb")
-    f.write(C)
+    f.write(ursina_networking_decompress_file(C))
     f.close()
     print("Done.")
