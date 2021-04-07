882f21020b68af5459444596c81ab1536158cb8e
Kevin <kevin.stoetzel@gmail.com>
2021-04-08 01:34:11 +0200

    removed examples

--- examples/send_files/send_file_server.py
+++
@@ -1,7 +0,0 @@
-from UrsinaNetworking import UrsinaNetworkingServer, ursina_networking_encode_file
-
-Server = UrsinaNetworkingServer("localhost", 25565)
-
-@Server.event
-def clientConnected(Ply):
-    Ply.send_message("ReceiveFile", ursina_networking_encode_file("image.png"))
--- examples/send_files/send_obj/send_obj_server.py
+++
@@ -1,8 +0,0 @@
-from UrsinaNetworking import UrsinaNetworkingServer, ursina_networking_encode_file
-
-Server = UrsinaNetworkingServer("localhost", 25565)
-
-@Server.event
-def requestFile(Ply, Datas):
-    print("Ok, no problem ! Sending file ...")
-    Ply.send_message("receiveFile", ursina_networking_encode_file("cone.obj"))
