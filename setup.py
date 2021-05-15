b79fa33c5fce0f2b48f5459d9c999bc34aeeb63d
Kevin <kevin.stoetzel@gmail.com>
2021-05-15 18:48:19 +0200

    ok i think its fixed now

--- setup.py
+++ setup.py
@@ -5,6 +5,7 @@ setup(
     version = "1.0.6",
     description = "A high level API for networking with the Ursina Engine",
     url = "https://github.com/kstzl/UrsinaNetworking",
+    author_email = "kevin.stoetzel@gmail.com",
     author = "K3#4869 & Squiggle#1385",
     license = "MIT",
     keywords = "ursina networking sockets",
