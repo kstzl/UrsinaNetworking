3acee6704b9302ad75bd53b01cce4e5b4bfa163a
Kevin <kevin.stoetzel@gmail.com>
2021-05-15 18:22:03 +0200

    pip

--- setup.py
+++ setup.py
@@ -6,6 +6,5 @@ setup(
     description = "A high level API for networking with the Ursina Engine",
     url = "https://github.com/kstzl/UrsinaNetworking",
     author = "K3#4869 & Squiggle#1385",
-    license = "MIT",
-    packages = find_packages()
+    license = "MIT"
 )
