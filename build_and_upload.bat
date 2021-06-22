640faed314497bf0e7d79b2470b38e56e623d3db
Kevin <kevin.stoetzel@gmail.com>
2021-06-22 17:29:59 +0200

    auto update

--- build_and_upload.bat
+++ build_and_upload.bat
@@ -1,5 +1,25 @@
+@echo off
+
+echo [1 / 4] Deleting old folders ...
+
+RMDIR "build" /s /q
+RMDIR "dist" /s /q
+RMDIR "UrsinaNetworking.egg-info" /s /q
+
+echo [2 / 4] Pushing on github ...
+
 git add *
 git commit -m "auto update"
 git push
+
+echo [3 / 4] Building the lib ...
+
 python setup.py sdist bdist_wheel
+
+echo [4 /4] Uploading to PyPi ...
+
 twine upload dist/*
+
+echo Finished ! :D
+
+pause
