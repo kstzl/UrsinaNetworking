bb90b8c49d02f73fcb702f7acaafc0eec8f058cb
kztzl <kevin.stoetzel@gmail.com>
2021-09-27 00:02:24 +0200

    script update

--- ursinanetworking/examples/replicated/shared.py
+++ ursinanetworking/examples/replicated/shared.py
@@ -21,11 +21,11 @@ class TestObject(Button, Replicator):
 
     def destroy_server(self):
         replicated_destroy(self)
-        rpc_multicast(self.sfx_multicast, 0.5)
+        self.rpc_multicast(self.sfx_multicast, 0.5)
 
     def place_server(self, pos):
         a = self.replicated_handler.create_replicated_object(TestObject, position = pos)
-        rpc_multicast(self.sfx_multicast, 1)
+        self.rpc_multicast(self.sfx_multicast, 1)
 
     def sfx_multicast(self, pitch):
         self.sfx.pitch = pitch
@@ -33,6 +33,6 @@ class TestObject(Button, Replicator):
 
     def input(self, key):
         if self.hovered and key == "left mouse down":
-            rpc_server(self.destroy_server)
+            self.rpc_server(self.destroy_server)
         if self.hovered and key == "right mouse down":
-            rpc_server(self.place_server, self.position + mouse.normal)
+            self.rpc_server(self.place_server, self.position + mouse.normal)
